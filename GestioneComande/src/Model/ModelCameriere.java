1b741e6e70fa926dca3e05576322062c5ab01082
glrk03 <marin.davide03@gmail.com>
2021-10-25 12:43:20 +0200

    gg

--- GestioneComande/src/Model/ModelCameriere.java
+++ GestioneComande/src/Model/ModelCameriere.java
@@ -1,5 +1,25 @@
 package Model;
 
-public class ModelCameriere {
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
+public class ModelCameriere {
+	private ArrayList <Comanda> comande;
+	
+	public ModelCameriere() {
+		comande=new ArrayList<Comanda>();
+	}
+	public void aggiungiComanda(Comanda c) {
+		comande.add(c);
+	}
+	public ArrayList<Comanda> getComande() {
+		return comande;
+	}
+	public void setComande(ArrayList<Comanda> comande){
+		comande.addAll(comande);
+		for(int i = 0; i < comande.size(); i++) {
+            System.out.println("comanda: "+comande.get(i).toString());
+        }
+	}
 }
